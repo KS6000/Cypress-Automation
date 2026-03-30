78e2fc9af235cdb3c62e2c5c265a62c376311652
KS6000 <kshura600@gmail.com>
2026-03-30 08:46:09 +0100

    Clean project structure and fix Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {},
   },
 });
